c3e828419214546dda71696e59d16ebaa8f4923f
ronchitech <ronchibusiness@gmail.com>
2021-06-27 00:35:46 +0800

    using middleware to protect routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,22 +3,22 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
-
+const routeProtection = require('../middleware/routeProtection')
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product',routeProtection, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/products',routeProtection, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product',routeProtection, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId',routeProtection, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', routeProtection,adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product',routeProtection, adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
-
+const routeProtection = require('../middleware/routeProtection')
 const router = express.Router();
 
 router.get('/', shopController.getIndex);
@@ -12,14 +12,18 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+router.get('/cart', routeProtection, shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+router.post('/cart', routeProtection, shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post(
+  '/cart-delete-item',
+  routeProtection,
+  shopController.postCartDeleteProduct
+);
 
-router.post('/create-order', shopController.postOrder);
+router.post('/create-order', routeProtection, shopController.postOrder);
 
-router.get('/orders', shopController.getOrders);
+router.get('/orders', routeProtection, shopController.getOrders);
 
 module.exports = router;
