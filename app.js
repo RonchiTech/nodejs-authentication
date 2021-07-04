b0594b57a48b918f05eacf3790796d6802f4d627
ronchitech <ronchibusiness@gmail.com>
2021-07-04 18:01:37 +0800

    serving images statically

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ const fileFilter = (req, file, cb) => {
     file.mimetype === 'image/jpg' ||
     file.mimetype === 'image/jpeg'
   ) {
-    cb(null,true);
+    cb(null, true);
   } else {
     cb(null, false);
-  } 
+  }
 };
 
 app.set('view engine', 'ejs');
@@ -59,7 +59,7 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/images',express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: 'my secret',
