8d673ac1028c8f5d06ce2ccc44477257c350cc9e
ronchitech <ronchibusiness@gmail.com>
2021-07-05 22:15:38 +0800

    adding client side code

---
+++ public/js/admin.js
@@ -0,0 +1,7 @@
+
+
+const deleteProductBtn = () => {
+    const productId = document.getElementById('productId').value;
+    const csrfToken = document.getElementById('csrfToken').value;
+    console.log(productId, csrfToken);
+}
